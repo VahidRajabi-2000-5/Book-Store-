49d1c812a3e619b562e81dfcc7c73623025520b3
vahid rajabi <vahidrajabi.software@gmail.com>
2025-02-26 23:30:37 +0330

    add BookDetailView

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse 
 
 class Book(models.Model):
     title = models.CharField(max_length=500)
@@ -9,3 +10,9 @@ class Book(models.Model):
     
     def __str__(self):
         return self.title
+    
+    
+    def get_absolute_url(self):
+        return reverse("book_detail", args=[self.id])
+        # return reverse("book_detail", kwargs={"pk": self.pk})
+    
--- books/views.py
+++ books/views.py
@@ -6,5 +6,11 @@ from . import models
 
 class BooksListView(generic.ListView):
     model = models.Book
-    template_name = 'books/books_list.html'
-    context_object_name = 'books'
+    template_name = "books/books_list.html"
+    context_object_name = "books"
+
+
+class BookDetailView(generic.DetailView):
+    model = models.Book
+    template_name = "books/book_detail.html"
+    context_object_name = "book"
