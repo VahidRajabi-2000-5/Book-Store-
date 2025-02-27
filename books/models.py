900c6f1886ef0fe794d09d9802d7cba63ed3e5dc
vahid rajabi <vahidrajabi.software@gmail.com>
2025-02-27 13:12:14 +0330

    add datetime create and modified in model book

--- books/models.py
+++ books/models.py
@@ -6,7 +6,8 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     content = models.TextField()
     price = models.DecimalField(max_digits=5, decimal_places=2)    
-    
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
     
     def __str__(self):
         return self.title
